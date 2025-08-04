927ef533b4f66966aa4cd8e0668ade80ccff230a
meemkoo <guy99992@gmail.com>
2025-08-04 13:50:12 -0500

    Simple config system begun

---
+++ addons/cvtmapgen/cvtmapgen.gd
@@ -0,0 +1,17 @@
+@tool
+extends EditorPlugin
+
+var rpanel = preload("res://addons/cvtmapgen/rpanel.tscn")
+var rpanel_inst: Control
+
+func _enter_tree() -> void:
+	print("Yeah we entering")
+	
+	rpanel_inst = rpanel.instantiate()
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, rpanel_inst)
+
+
+func _exit_tree() -> void:
+	#rpanel_inst.save()
+	remove_control_from_docks(rpanel_inst)
+	rpanel_inst.queue_free()
---
+++ addons/cvtmapgen/rpanel.gd
@@ -0,0 +1,48 @@
+@tool
+extends Control
+
+const CFG_PATH = "res://cvtm.json"
+var cfg: Dictionary
+
+var inputs: Dictionary = {
+	"mapsources": null
+}
+
+func _on_save_pressed() -> void:
+	save()
+
+func _init() -> void:
+	var file
+	if FileAccess.file_exists(CFG_PATH):
+		file = FileAccess.open(CFG_PATH, FileAccess.READ)
+		cfg = JSON.parse_string(file.get_as_text())
+	else:
+		cfg = {}
+		file = FileAccess.open(CFG_PATH, FileAccess.WRITE)
+		file.store_string(JSON.stringify(cfg))
+	file.close()
+	child_entered_tree.connect(handle_child)
+
+func assign_data(node: Node, data):
+	if node is TextEdit:
+		node.text = data
+
+func handle_child(node: Node):
+	if node.name in inputs.keys():
+		inputs[node.name] = node
+		
+	if node.name in cfg.keys():
+		if cfg[node.name]:
+			assign_data(node, cfg[node.name])
+
+func save() -> void:
+	print("Saving")
+	print(inputs["mapsources"].text)
+	cfg["mapsources"] = inputs["mapsources"].text
+	print(cfg["mapsources"])
+	var file = FileAccess.open(CFG_PATH, FileAccess.WRITE)
+	file.store_string(JSON.stringify(cfg))
+
+
+func _on_open_terminal() -> void:
+	OS.execute("C:\\Program Files\\Git\\git-bash.exe", ["--cd-to-home"])
