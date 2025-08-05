46f33e47b6056d8fbd3143fbd3d913f69c9864a2
meemkoo <guy99992@gmail.com>
2025-08-04 21:57:35 -0500

    Thes commits dont have to mean anything right? Right?

--- addons/cvtmapgen/cvtmapgen.gd
+++ addons/cvtmapgen/cvtmapgen.gd
@@ -3,13 +3,17 @@ extends EditorPlugin
 
 var rpanel = preload("res://addons/cvtmapgen/rpanel.tscn")
 var rpanel_inst: Control
+var rsc: AcceptDialog
+
 
 func _enter_tree() -> void:
 	rpanel_inst = rpanel.instantiate()
+	rsc = rpanel_inst.get_node("ReqSaveConfig")
+	# rsc.visible = false
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, rpanel_inst)
 
 
 func _exit_tree() -> void:
-	#rpanel_inst.save()
+	# rsc.visible = true
 	remove_control_from_docks(rpanel_inst)
 	rpanel_inst.queue_free()
